fed9d16be4b4cecda642bd17aae90253b58f82e2
Asmat1k <dobrovti@mail.ru>
2026-05-10 18:07:44 +0300

    TR-1 - fixed ioc

--- src/common/ioc.ts
+++ src/common/ioc.ts
@@ -1,7 +1,12 @@
 import { Container } from "inversify"
+import { iocCommonContainer } from "./ioc-module"
 
-export const iocСommonContainer = new Container({})
+export const commonContainer = new Container({})
+
+if (!iocCommonContainer.id) {
+  commonContainer.load(iocCommonContainer)
+}
 
 export function useCommonIoCBinding<T>(identifier: symbol): T {
-  return iocСommonContainer.get<T>(identifier)
+  return commonContainer.get<T>(identifier)
 }
--- src/unit/chat/js/ioc.ts
+++ src/unit/chat/js/ioc.ts
@@ -1,8 +1,14 @@
-import { iocCommonContainer } from "@common"
 import { Container } from "inversify"
+import { commonContainer } from "@common"
+import { iocChatContainer } from "./ioc-module"
 
-export const iocContainer = new Container({})
-iocContainer.load(iocCommonContainer)
+export const iocContainer = new Container({
+  parent: commonContainer,
+})
+
+if (!iocChatContainer.id) {
+  iocContainer.load(iocChatContainer)
+}
 
 export function useIoCBinding<T>(identifier: symbol): T {
   return iocContainer.get<T>(identifier)
