dbece20e868ec6bb95fa56903b753d28c1c05a46
heavygendalf <heavygendalf@gmail.com>
2026-05-14 21:06:58 +0300

    TR-3 - no one saw anything

--- src/unit/chat/jslazy/ChatLazyComponent.tsx
+++ src/unit/chat/jslazy/ChatLazyComponent.tsx
@@ -1,10 +1,8 @@
 import { lazy } from "react"
-import { iocCommonContainer } from "@common"
 
 export const ChatLazyComponent = lazy(() => {
   return import("../js").then((module) => {
-    const { iocChatContainer, iocContainer, ChatComponent } = module
-    iocContainer.load(iocCommonContainer)
+     const { iocChatContainer, iocContainer, ChatComponent } = module
     iocContainer.load(iocChatContainer)
 
     return { default: ChatComponent }
