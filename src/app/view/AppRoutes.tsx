545975ff1aa96c925895ee8049508d075735e28d
Asmat1k <dobrovti@mail.ru>
2026-05-15 18:15:05 +0300

    TR-11 - made one route

--- src/app/view/AppRoutes.tsx
+++ src/app/view/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider } from "react-router-dom"
 import { observer } from "mobx-react-lite"
-import { Paths } from "@common"
+import { defaultPath } from "@common"
 import { ChatLazyComponent } from "@unit/chat/jslazy"
 
 export const AppRoutes = observer(function AppRoutes() {
@@ -8,9 +8,7 @@ export const AppRoutes = observer(function AppRoutes() {
   const router = createBrowserRouter(
     createRoutesFromChildren(
       <>
-        <Route path={Paths.path + "/"} element={<div>Hello, World!</div>}/>
-        <Route path={Paths.chat.path + "/"} element={<ChatLazyComponent/>}/>
-        <Route path={Paths.auth.path + "/"} element={<div>Auth page</div>}/>
+        <Route path={defaultPath + "*"} element={<ChatLazyComponent/>}/>
       </>
     )
   )
--- src/common/common/Path.ts
+++ src/common/common/Path.ts
@@ -1,11 +1,5 @@
 export const Paths = {
   path: "/",
-  chat: {
-    path: "chat",
-  },
-  auth: {
-    path: "auth",
-  },
 }
 
 export const defaultPath = Paths.path
